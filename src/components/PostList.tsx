a57b2446ecc1876fb847b87b216104fd99f17ea6
miloe2 <miloe0117@gmail.com>
2025-07-04 11:40:53 +0900

    Refact : PostList 분리

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,51 +1,12 @@
-import { useRef } from "react";
-import { CircularProgress } from "@mui/material";
-import PostItem from "./PostItem";
-import { useGetPosts } from "../hooks/queries/useGetPosts";
-import { useIntersectionObserver } from "../hooks/useIntersectionObserver";
-import { useNavigate } from "react-router-dom";
+import PostItem from './PostItem';
+import type { Post } from '../types';
 
-function PostList() {
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
-    useGetPosts();
-  const allPosts = data?.pages.flatMap((page) => page.posts) ?? [];
-  const timerRef = useRef<NodeJS.Timeout | null>(null);
-  const navigate = useNavigate();
-
-  const handleClickPost = (id: number) => {
-    navigate(`/posts/${id}`);
-  };
-
-  const loadMoreRef = useIntersectionObserver({
-    onIntersect: () => {
-      if (hasNextPage && !isFetchingNextPage) {
-        if (timerRef.current) clearTimeout(timerRef.current);
-        timerRef.current = setTimeout(() => {
-          fetchNextPage();
-        }, 1000);
-      }
-    },
-    enabled: hasNextPage,
-    threshold: 0.1,
-  });
-
+function PostList({ posts, onClick }: { posts: Post[]; onClick: (id: number) => void }) {
   return (
     <>
-      {allPosts?.map((post) => (
-        <PostItem
-          key={post.id}
-          post={post}
-          onClick={() => {
-            console.log(post.id);
-            handleClickPost(post.id);
-          }}
-        />
+      {posts.map((post) => (
+        <PostItem key={post.id} post={post} onClick={() => onClick(post.id)} />
       ))}
-      <div ref={loadMoreRef} style={{ minHeight: 20, marginTop: 10 }}>
-        {!isFetchingNextPage && (
-          <CircularProgress size={40} color="info" thickness={5} />
-        )}
-      </div>
     </>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import { Toolbar } from "@mui/material";
 import Navigation from "../components/Navigation";
-import PostList from "../components/PostList";
+import PostListContainer from "../components/PostListContainer";
 
 function Home() {
   return (
     <>
       <Navigation />
       <Toolbar />
-      <PostList />
+      <PostListContainer />
     </>
   );
 }
